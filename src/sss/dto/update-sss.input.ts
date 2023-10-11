9aad8e3ba76c4fc79443adaef34b4921a55d8b10
lyk990 <1477435414@qq.com>
2023-10-11 21:50:13 +0800

    modules

--- src/sss/dto/update-sss.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateSssInput } from './create-sss.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateSssInput extends PartialType(CreateSssInput) {
-  @Field(() => Int)
-  id: number;
-}
--- src/sss/sss.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SssService } from './sss.service';
-import { SssResolver } from './sss.resolver';
-
-@Module({
-  providers: [SssResolver, SssService]
-})
-export class SssModule {}
